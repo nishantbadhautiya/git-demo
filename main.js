47498c262998c1212ffeb59a05049678a440336e
Nishant Badhautiya <nishantbadhautiya100@gmail.com>
2026-01-06 09:05:20 +0000

    commit#102

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ console.log('new change in initial commit');
 console.log('new change in initial commit in main branch')
 console.log('Branch: Main, Commit: commit#1 ')
 console.log('Branch: branch01, commit: commit#101')
+console.log('this is a simple change')
+console.log('Branch: branch01, commit: commit#102')
---
+++ main2.js
@@ -0,0 +1,10 @@
+console.log('new file');
+console.log('added another line in new file');
+console.log('final change in new file');
+
+console.log('change after staging the file');
+console.log('another change after staging the file');
+
+console.log('change after committing the staged file');
+console.log('another change after committing the staged file');
+console.log("this is a simple change #1")
